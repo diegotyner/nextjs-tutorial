77f7f7e41bef6eee837cdff8dffbe784763f0dd4
diegotyner <120358477+diegotyner@users.noreply.github.com>
2024-07-23 00:37:22 -0700

    No cache attempt again

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -5,6 +5,7 @@ export const GET = async (req: Request) => {
   try {
     await connectToDB();
     const prompts = await Prompt.find({}).populate('creator')
+    console.log("I am actually running")
 
     return new Response(JSON.stringify(prompts), { status: 200,   headers: { 'Cache-Control': 'no-store' }, })
   } catch (error) {
