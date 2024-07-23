403f9cb7f2ebe84f0c14cd918f1753b92318216c
diegotyner <120358477+diegotyner@users.noreply.github.com>
2024-07-23 00:33:18 -0700

    No cache attempt again

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -4,10 +4,10 @@ import Prompt from "@/models/prompt"
 export const GET = async (req: Request) => {
   try {
     await connectToDB();
-    const prompts = await Prompt.find({}).populate('creator');
+    const prompts = await Prompt.find({}).populate('creator')
 
-    return new Response(JSON.stringify(prompts), { status: 200 })
+    return new Response(JSON.stringify(prompts), { status: 200,   headers: { 'Cache-Control': 'no-store' }, })
   } catch (error) {
-    return new Response("Failed to fetch all prompts", { status: 200 })
+    return new Response("Failed to fetch all prompts", { status: 500 })
   }
 }
