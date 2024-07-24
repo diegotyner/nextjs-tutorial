080ef1d956ca31a616359482bc01a10081763789
diegotyner <120358477+diegotyner@users.noreply.github.com>
2024-07-24 15:19:57 -0700

    Fixed!

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -6,9 +6,8 @@ import { connectToDB } from "@/utils/database";
 export const GET = async (req: Request) => {
   try {
     await connectToDB();
-    console.log("Fetching prompts...");
     const prompts = await Prompt.find({}).populate("creator", "email username image", User)
-
+    console.log("Fetched prompts successfully");
     return new Response(JSON.stringify(prompts), { status: 200,   headers: { 'Cache-Control': 'no-store' }, })
   } catch (error) {
     console.log("Error fetching prompts:", error);
